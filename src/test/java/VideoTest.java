bd8b20d061b3080a7bea3ed97e7f22b2f664f09f
leohcavalcanti <leonardocaturyty@hotmail.com>
2022-06-09 09:21:40 -0300

    Test correction

--- src/test/java/VideoTest.java
+++ src/test/java/VideoTest.java
@@ -1,8 +1,7 @@
-import business.ControllerVideo;
+import business.VideoController;
 import business.entity.Movie;
 import business.entity.Video;
 import business.exceptions.*;
-import data.IRepositoryVideo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,8 +12,8 @@ import java.util.ArrayList;
 public class VideoTest {
 
     private ArrayList<Video> midias;
-    private int cont = 0;
-    private ControllerVideo controllerVideo = new ControllerVideo();
+    private int count = 0;
+    private VideoController VideoController = new VideoController();
 
     @BeforeEach
     public void setup(){
@@ -22,32 +21,32 @@ public class VideoTest {
     }
 
     @Test
-    public void esperaAumentoMidias(){
-        controllerVideo.createMovie("It", 160, "Terror", 2017, "Warn");
-        Assertions.assertEquals(1, controllerVideo.getVideos());
+    public void esperaAumentoMidias() throws CantCreateMovieException{
+        VideoController.createMovie("It", 160, "Terror", 2017, "Warn");
+        Assertions.assertEquals(1, VideoController.getVideos());
     }
 
     @Test
-    public void esperaIncrementoMidias(){
-        controllerVideo.createMovie("It", 160, "Terror", 2017, "Warn");
-        Assertions.assertEquals(1, controllerVideo.getVideos());
+    public void esperaIncrementoMidias() throws CantCreateMovieException{
+        VideoController.createMovie("It", 160, "Terror", 2017, "Warn");
+        Assertions.assertEquals(1, VideoController.getVideos());
     }
 
     @Test
-    public void esperaPlayVideo() throws VideoAlreadyPlayingException, VideoNotFoundException, EpisodeCantAdvanceException, EpisodeNotFoundException, ListOfEpisodesEmptyException {
-        controllerVideo.createMovie("It", 160, "Terror", 2017, "Warn");
-        controllerVideo.play("It");
+    public void esperaPlayVideo() throws VideoAlreadyPlayingException, VideoNotFoundException, EpisodeCantAdvanceException, EpisodeNotFoundException, ListOfEpisodesEmptyException, CantCreateMovieException {
+        VideoController.createMovie("It", 160, "Terror", 2017, "Warn");
+        VideoController.play("It");
 
-        Video movieFind = controllerVideo.find("It");
+        Video movieFind = VideoController.find("It");
         Assumptions.assumeTrue(movieFind.getPlaying());
     }
 
     @Test
-    public void esperaPauseVideo() throws VideoAlreadyPlayingException, VideoNotFoundException, EpisodeNotFoundException, ListOfEpisodesEmptyException {
-        controllerVideo.createMovie("It", 160, "Terror", 2017, "Warn");
-        controllerVideo.play("It");
+    public void esperaPauseVideo() throws VideoAlreadyPlayingException, VideoNotFoundException, EpisodeNotFoundException, ListOfEpisodesEmptyException, CantCreateMovieException {
+        VideoController.createMovie("It", 160, "Terror", 2017, "Warn");
+        VideoController.play("It");
 
-        Video movieFind = controllerVideo.find("It");
+        Video movieFind = VideoController.find("It");
         Assumptions.assumeFalse(movieFind.getPlaying());
     }
 
